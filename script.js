30452daff05bac8527ab09313108a5147b1907e0
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2022-12-11 14:35:04 +0000

    changed the HTML. Changed to textarea

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-$(document).ready(function() {
-//Todays date
-    let displayDate = $("#currentDay");
-    displayDate.text(moment().format("dddd MMMM Do"));
-// Current Hour
-    let currentHour = moment().format("HH");
-
+$(document).ready(function () {
+  //Todays date
+  let displayDate = $("#currentDay");
+  displayDate.text(moment().format("dddd MMMM Do"));
+  // Current Hour
+  let currentHour = moment().format("HH");
+  // Logic for changing the colours 
+  
 });
