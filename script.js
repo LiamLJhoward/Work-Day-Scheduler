5be70e8ea3f2899ae56eaf3596dda09fa01e4ee2
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2023-02-08 12:18:03 +0000

    localstorage complete

--- script.js
+++ script.js
@@ -6,25 +6,41 @@ $(document).ready(function () {
   // Current Hour
   let currentHour = moment().format("HH");
 
-for (let i = 9; i < 18; i++) {
+  // Save Button
+  let saveBtn = $(".saveBtn");
+
+  // Colours time blocks
+  for (let i = 9; i < 18; i++) {
+    let hour = parseInt($(`#${i}`).attr("id"));
+    console.log(currentHour, hour);
+    if (currentHour == hour) {
+      $(`#${i}`).addClass("present");
+    } else if (currentHour < hour) {
+      $(`#${i}`).addClass("future");
+    } else {
+      $(`#${i}`).addClass("past");
+    }
+
+    // localStorage getItem here
+    saveBtn.on("click", function () {
+      let time = $(this).siblings(".hour").text();
+      let schedule = $(this).siblings(".schedule").val();
+
+      localStorage.setItem(time, schedule);
+    });
+
+    function usePlanner() { 
+
+      $(".hour").each(function() {
+        let current = $(this).text();
+        let currentPlan = localStorage.getItem(current);
+
+        if(currentPlan !== null) {
+          $(this).siblings(".schedule").val(currentPlan);
+        }
+      }
+ )}
+ usePlanner();
+    }});
+
    
-  let hour = parseInt($(`#${i}`).attr("id"))
-  console.log(currentHour, hour)
-  if (currentHour == hour) {
-    $(`#${i}`).addClass("present");
-  } else if (currentHour < hour) {
-    $(`#${i}`).addClass("future");
-  } else {
-    $(`#${i}`).addClass("past");
-  }
-  // localStorage getItem here
-  
-$(".saveBtn").on("click", function(){
- let text = $("textarea").attr(".textarea");
- localStorage.setItem(hour, JSON.stringify(text));
-}); 
-  
-}
-
-
-});
